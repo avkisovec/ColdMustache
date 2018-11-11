8f514803fffec3157f0a899aefd2fa179d1cc3f9
avkisovec <michael.machacek@spsrakovnik.com>
2018-11-11 15:14:55 +0100

    úklid

---
+++ ColdMustache/Assets/Resources/Entities/Damager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damager : MonoBehaviour {
+
+    public Entity.team Team;
+    public int Damage = 1;
+    
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        Entity hit = coll.GetComponent<Entity>();
+        if (hit!=null)
+        {
+            if(Team != hit.Team)
+            {
+                hit.TakeDamage(1);
+                GetComponent<DeathAnimation>().Spawn();
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
