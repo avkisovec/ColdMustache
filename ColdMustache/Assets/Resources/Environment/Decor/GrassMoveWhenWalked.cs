5136b2b8279c8a9a5695cd5a741b2f54bb58ef42
avkisovec <michael.machacek@spsrakovnik.com>
2018-12-03 16:55:02 +0100

    interakce s prostredim - trava, stopy

---
+++ ColdMustache/Assets/Resources/Environment/Decor/GrassMoveWhenWalked.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassMoveWhenWalked : MonoBehaviour {
+
+    public float AngleLimit = 60;
+    public float AngularVelocity = 15; //maybe per second
+    public float BaseAngularVelocityRemaining;
+    float AngularMovementRemaining = 0;
+    bool IsMovementNegative = false;
+    float TargetAngle = 0; //this is to avoid modulo problems with transform's rotation
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(1 * Time.deltaTime*60);
+        if(AngularMovementRemaining > 0)
+        {
+            float ThisFrameAngularVelocity = AngularVelocity * Time.deltaTime;
+            AngularMovementRemaining -= ThisFrameAngularVelocity;
+
+
+            //positive movement
+            if (!IsMovementNegative)
+            {
+                if (TargetAngle < AngleLimit)
+                {
+                    TargetAngle += ThisFrameAngularVelocity;
+                }
+                else
+                {
+                    IsMovementNegative = true;
+                }
+            }
+            else if (IsMovementNegative)
+            {
+                if (TargetAngle > -AngleLimit)
+                {
+                    TargetAngle -= ThisFrameAngularVelocity;
+                }
+                else
+                {
+                    IsMovementNegative = false;
+                }
+            }
+
+
+
+            transform.rotation = Quaternion.Euler(0, 0, TargetAngle);
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        Entity hit = coll.GetComponent<Entity>();
+        if (hit != null)
+        {
+            AngularMovementRemaining+=BaseAngularVelocityRemaining;
+            //coll.gameObject.AddComponent<FxFootSteps>().ini(LifespanInSeconds, CooldownInSeconds, sprite, color);
+        }
+    }
+}
---
+++ ColdMustache/Assets/Resources/Fx/Permanent/FxFootSteps.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FxFootSteps : MonoBehaviour {
+
+    public float LifespanInSeconds = 3.5f;
+    public float CooldownInSeconds = 1;
+    public float CurrCooldown = 0;
+    public Sprite sprite;
+    public Color color;
+    
+	// Update is called once per frame
+	void Update () {
+        LifespanInSeconds -= Time.deltaTime;
+        if(LifespanInSeconds < 0)
+        {
+            Destroy(this);
+            return;
+        }
+        if(CurrCooldown > 0)
+        {
+            CurrCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            CurrCooldown = CooldownInSeconds;
+
+            GameObject footStep = new GameObject();
+            footStep.transform.position = transform.position;
+            footStep.transform.rotation = Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360)));
+
+            footStep.AddComponent<SpriteRenderer>().sprite = sprite;
+            footStep.GetComponent<SpriteRenderer>().color = color;
+
+            footStep.AddComponent<DieInSeconds>().Seconds = 20;
+
+        }
+	}
+
+    public void ini(float LifespanInSeconds, float CooldownInSeconds, Sprite sprite, Color color)
+    {
+        this.LifespanInSeconds = LifespanInSeconds;
+        this.CooldownInSeconds = CooldownInSeconds;
+        this.sprite = sprite;
+        this.color = color;
+    }
+}
