8ac46a000f582cc36ac04bbcff283445210780d1
avkisovec <michael.machacek@spsrakovnik.com>
2019-02-24 18:42:46 +0100

    terrain builder

---
+++ ColdMustache/Assets/Resources/Environment/Decor/Terrain/TerrainBuilderGrasslike.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainBuilderGrasslike : MonoBehaviour
+{
+    public bool ENABLED = true;
+    public Texture2D Map;
+
+    public string LowDensitySheetPath;
+    public string MediumDensitySheetPath;
+    public string HighDensitySheetPath;
+    public string VEryHighDensitySheetPath;
+
+    public float Zposition = 230;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!ENABLED) return;
+
+        GenerateGrassLayer();
+        
+        ENABLED = false;
+    }
+
+    void GenerateGrassLayer()
+    {
+        UnityEngine.Color[] all = Map.GetPixels();
+
+        Sprite[] GrassSpritesLow = Resources.LoadAll<Sprite>(LowDensitySheetPath);
+        Sprite[] GrassSpritesMedium = Resources.LoadAll<Sprite>(MediumDensitySheetPath);
+        Sprite[] GrassSpritesHigh = Resources.LoadAll<Sprite>(HighDensitySheetPath);
+        Sprite[] GrassSpritesVeryHigh = Resources.LoadAll<Sprite>(VEryHighDensitySheetPath);
+
+        for (int y = 0; y < Map.height; y++)
+        {
+            for (int x = 0; x < Map.width; x++)
+            {
+                if (all[y * Map.width + x].a != 0)
+                {
+
+                    int Connections = 0;
+                    //right
+                    if (IsTileWithinMapBounds(x + 1, y)) { if (all[y * Map.width + x + 1].a != 0) { Connections++; } }
+                    //right up
+                    if (IsTileWithinMapBounds(x + 1, y + 1)) { if (all[(y + 1) * Map.width + x + 1].a != 0) { Connections++; } }
+                    //up
+                    if (IsTileWithinMapBounds(x, y + 1)) { if (all[(y + 1) * Map.width + x].a != 0) { Connections++; } }
+                    //left up
+                    if (IsTileWithinMapBounds(x - 1, y + 1)) { if (all[(y + 1) * Map.width + x - 1].a != 0) { Connections++; } }
+                    //left
+                    if (IsTileWithinMapBounds(x - 1, y)) { if (all[y * Map.width + x - 1].a != 0) { Connections++; } }
+                    //left down
+                    if (IsTileWithinMapBounds(x - 1, y - 1)) { if (all[(y - 1) * Map.width + x - 1].a != 0) { Connections++; } }
+                    //down
+                    if (IsTileWithinMapBounds(x, y - 1)) { if (all[(y - 1) * Map.width + x].a != 0) { Connections++; } }
+                    //right down
+                    if (IsTileWithinMapBounds(x + 1, y - 1)) { if (all[(y - 1) * Map.width + x + 1].a != 0) { Connections++; } }
+
+                    Connections += Random.Range(-1, 2);
+
+
+
+                    GameObject grass = new GameObject();
+                    grass.AddComponent<SpriteRenderer>().color = all[y * Map.width + x];
+                    grass.transform.position = new Vector3(x, y, Zposition);
+
+                    if (Connections > 8)
+                    {
+                        grass.GetComponent<SpriteRenderer>().sprite = GrassSpritesVeryHigh[Random.Range(0, GrassSpritesVeryHigh.Length)];
+                    }
+                    else if (Connections > 4)
+                    {
+                        grass.GetComponent<SpriteRenderer>().sprite = GrassSpritesHigh[Random.Range(0, GrassSpritesHigh.Length)];
+                    }
+                    else if (Connections > 2)
+                    {
+                        grass.GetComponent<SpriteRenderer>().sprite = GrassSpritesMedium[Random.Range(0, GrassSpritesMedium.Length)];
+                    }
+                    else
+                    {
+                        grass.GetComponent<SpriteRenderer>().sprite = GrassSpritesLow[Random.Range(0, GrassSpritesLow.Length)];
+                    }
+
+
+                }
+            }
+        }
+    }
+
+
+    bool IsTileWithinMapBounds(int x, int y)
+    {
+        if (x >= 0 &&
+            x < Map.width &&
+            y >= 0 &&
+            y < Map.height
+            )
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ ColdMustache/Assets/Resources/Environment/Decor/Terrain/TerrainBuilderGrasslike2.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainBuilderGrasslike2 : MonoBehaviour
+{
+    public bool ENABLED = true;
+    public Texture2D Map;
+
+    //ordered from lowest density to highest density
+    //lowest density will be used around edges, highest density more in center
+    public string[] SpriteSheetPaths;
+
+    public float Zposition = 230;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!ENABLED) return;
+
+        GenerateGrassLayer();
+
+        ENABLED = false;
+    }
+
+    void GenerateGrassLayer()
+    {
+        UnityEngine.Color[] all = Map.GetPixels();
+
+        Sprite[][] SpriteSheets = new Sprite[SpriteSheetPaths.Length][];
+        for(int i = 0; i < SpriteSheets.Length; i++){
+            SpriteSheets[i] = Resources.LoadAll<Sprite>(SpriteSheetPaths[i]);
+        }
+
+        for (int y = 0; y < Map.height; y++)
+        {
+            for (int x = 0; x < Map.width; x++)
+            {
+                if (all[y * Map.width + x].a != 0)
+                {
+
+                    #region FindingDistanceFromEdge
+
+
+                    int MaxDistanceToCheck = 2*SpriteSheets.Length;
+                    int MyDistanceFromEdge = -1;
+
+                    Vector2Int Curr = new Vector2Int(x, y);
+
+                    int CurrLength = 1;
+                    int DirectionPointer = 0;
+                    Vector2Int[] directions = new Vector2Int[] {
+                        new Vector2Int(1,0),
+                        new Vector2Int(0,1),
+                        new Vector2Int(-1,0),
+                        new Vector2Int(0,-1)
+                    };
+
+                    //output.SetPixel(Curr.x, Curr.y, palette[0]);
+
+                    for (int i = 0; i < MaxDistanceToCheck; i++)
+                    {
+                        for (int Doubler = 0; Doubler < 2; Doubler++)
+                        {
+
+                            for (int length = 0; length < CurrLength; length++)
+                            {
+                                Curr = Curr + directions[DirectionPointer];
+                                
+                                if(IsTileWithinMapBounds(Curr.x,Curr.y)){
+                                    if(all[Curr.y * Map.width + Curr.x].a == 0){
+                                        //edge was found
+                                        MyDistanceFromEdge = Mathf.Abs(Curr.x-x) + Mathf.Abs(Curr.y-y);
+                                        goto End;
+                                    }
+                                }
+                            }
+
+                            DirectionPointer++;
+                            DirectionPointer = DirectionPointer % directions.Length;
+
+                        }
+
+                        CurrLength++;
+                    }
+                    //if no edge was found, go for maxvalue (keep in mind theres -- later)
+                    MyDistanceFromEdge = SpriteSheets.Length;
+                    
+                    End:;
+
+
+                    MyDistanceFromEdge--;
+                    if(MyDistanceFromEdge < 0) MyDistanceFromEdge = 0;
+                    if(MyDistanceFromEdge >= SpriteSheets.Length) MyDistanceFromEdge = SpriteSheets.Length-1;
+
+
+                    #endregion;
+
+
+
+                    GameObject grass = new GameObject();
+                    grass.AddComponent<SpriteRenderer>().color = all[y * Map.width + x];
+                    grass.transform.position = new Vector3(x, y, Zposition);
+                    grass.name = MyDistanceFromEdge.ToString();
+
+                    grass.GetComponent<SpriteRenderer>().sprite = SpriteSheets[MyDistanceFromEdge][Random.Range(0, SpriteSheets[MyDistanceFromEdge].Length)];
+
+                    grass.transform.parent = transform;
+                    
+
+                }
+            }
+        }
+    }
+
+
+
+    bool IsTileWithinMapBounds(int x, int y)
+    {
+        if (x >= 0 &&
+            x < Map.width &&
+            y >= 0 &&
+            y < Map.height
+            )
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
